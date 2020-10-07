675bc8525b26f8ed55fe13ef7e32307b41a247b8
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-10-07 15:12:43 +0200

    Submodule 27.5 endpoints2

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
+const db = require('./db.js');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid'); //Do losowania id możesz użyć zewnętrznej biblioteki, np. uuid.
 const bodyParser = require('body-parser');
 
+console.log('db PLIK:', db.db.testimonials);
 const app = express(); 
 app.use(express.urlencoded({ extended: false }));//Jeśli chcesz mieć obsługę formularzy x-www-form-urlencoded, dodaj middleware express.urlencoded.
 app.use(express.json()); //Jeśli dodatkowo chcesz odbierać dane w formacie JSON (mogą być wysyłane za pomocą form-data), to również express.json.
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));  // to support URL-encoded bodies
   
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-  { id: 3, author: 'Dave Gahan', text: 'Speak & Spell.' },
-  { id: 4, author: 'Martin Gore', text: 'Enjoy the Silence.' },
-  { id: 5, author: 'Alan Wilder', text: 'Walking in my shoes.' },
-  { id: 6, author: 'Vince Clarke', text: 'A question of time.' },
-];
-
 app.get('/testimonials', (req, res) => { //ma zwracać całą zawartość tablicy. W formacie JSON - https://expressjs.com/en/api.html
-  res.json(db);
+  res.json(db.db.testimonials);
 });
 
 function getRandomInt(min, max) {
@@ -28,12 +21,12 @@ function getRandomInt(min, max) {
 }
 
 app.get('/testimonials/random', (req, res) => { //zwracamy losowy element z tablicy. Do losowania id możesz użyć zewn. biblioteki, np. uuid.
-  let id = getRandomInt(0, db.length -1);
-  res.json(db[id]);
+  let id = getRandomInt(0, db.db.testimonials.length -1);
+  res.json(db.db.testimonials[id]);
 });
 
 app.get('/testimonials/:id', (req, res) => { //zwracamy tylko jeden element tablicy, zgodny z :id.
-  res.json(db[req.params.id -1]); //https://expressjs.com/en/api.html#res
+  res.json(db.db.testimonials[req.params.id -1]); //https://expressjs.com/en/api.html#res
 });
 
 function changeAuthorAndText(id, author, text) { //https://stackoverflow.com/questions/4689856/how-to-change-value-of-object-which-is-inside-an-array-using-javascript-or-jquer/50504081
@@ -47,8 +40,7 @@ function changeAuthorAndText(id, author, text) { //https://stackoverflow.com/que
 }
 
 app.put('/testimonials/:id', (req, res) => {//modyfikujemy atrybuty author i text elementu tablicy o pasującym :id. Załóż, że body otrzymane w requeście będzie obiektem z atrybutami author i text.
-  changeAuthorAndText(db[req.params.id -1].id, req.body.author, req.body.text);
-  console.log('dbAfterChange:', db);
+  changeAuthorAndText(db.db.testimonials[req.params.id -1].id, req.body.author, req.body.text);
   res.json({ message: 'OK' });
 });
 
@@ -58,12 +50,12 @@ app.post('/testimonials', (req, res) => {//dodajemy nowy element do tablicy. Bod
   let newText = req.body.text;
   let newObject = {};
   newObject = {id: newId, author: newAuthor, text: newText};
-  db.push(newObject);
+  db.db.testimonials.push(newObject);
   res.json({ message: 'OK' }); 
 });
 
 app.delete('/testimonials/:id', (req, res) => {//usuwamy z tablicy wpis o podanym id.
-  db.splice(req.params.id -1, 1);
+  db.db.testimonials.splice(req.params.id -1, 1);
   res.json({ message: 'OK' });
 });
 
